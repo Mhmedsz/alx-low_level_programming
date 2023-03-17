f9380409f0b30c8f587da5a55c19127ec8bdad2c
mohameds <mohmedahmed557766@gmail.com>
2023-03-16 19:56:46 -0700

    mmmmm

---
+++ 0x00-hello_world/101-quote.c
@@ -0,0 +1,17 @@
+#include
+#include
+main -
+rror
+< stdio.h>
+< unistd.h>
+A c program that prints a
+• Return 1 (Success)
+main(void)
+int
+fput(
+par,
+return (I);
+line to the standard e
+Dora Kor
+that piece of art
+stdout
