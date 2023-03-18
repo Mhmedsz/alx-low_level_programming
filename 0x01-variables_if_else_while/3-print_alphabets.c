4ecb8b4b333ba81ecb72e3e41293296c85b3cc65
mohameds <mohmedahmed557766@gmail.com>
2023-03-17 18:58:21 -0700

    print_alphabet

--- 0x01-variables_if_else_while/3-print_alphabets.c
+++ 0x01-variables_if_else_while/3-print_alphabets.c
@@ -9,14 +9,14 @@
 
 int main(void)
 {
-        char letters[] = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ";
+	char letters[] = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ";
 
-        int i;
+	int i;
 
-        for (i = 0; i < 52; i++)
-        {
-                putchar (letters[i]);
-        }
-        putchar('\n');
-        return (0);
+	for (i = 0; i < 52; i++)
+	{
+		putchar(letters[i]);
+	}
+	putchar('\n');
+	return (0);
 }
