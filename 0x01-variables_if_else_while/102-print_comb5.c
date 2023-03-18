bc016f17c4c94d6af0a399dabc379097c47ff237
mohameds <mohmedahmed557766@gmail.com>
2023-03-18 15:16:24 -0700

    all possible combinations of two two-digit numbers

--- 0x01-variables_if_else_while/102-print_comb5.c
+++ 0x01-variables_if_else_while/102-print_comb5.c
@@ -21,7 +21,7 @@ int main(void)
 			putchar (i + '0');
 			putchar (i + '0');
 			putchar (' ');
-			putchar (j + '0');
+			putchar (i + '0');
 			putchar (j + '0');
 			if (i != 98 || j != 99)
 			{
