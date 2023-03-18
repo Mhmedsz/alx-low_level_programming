3dd80ed106c831e9f7572a3b4a28cf5432924f14
mohameds <mohmedahmed557766@gmail.com>
2023-03-17 18:41:51 -0700

    print_alphabet

---
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,22 @@
+#include <stdlib.h>
+#include <time.h>
+#include <stdio.h>
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 (Success)
+*/
+
+int main(void)
+{
+	char letters[] = "abcdefghijklmnopqrstuvwxyz";
+
+	int i;
+
+	for (i = 0; i <= 26; i++)
+	{
+		putchar (letters[i]);
+	}
+	puchar('\n');
+	return (0);
+}
