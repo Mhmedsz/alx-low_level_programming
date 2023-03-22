7aea12728547ee99c9d62527c75db1a035db8170
mohameds <mohmedahmed557766@gmail.com>
2023-03-21 23:52:17 -0700

    fibonacci

--- 0x02-functions_nested_loops/102-fibonacci.c
+++ 0x02-functions_nested_loops/102-fibonacci.c
@@ -9,18 +9,18 @@ int main(void)
 {
 	int i, f1 = 0, f2 = 1, sum;
 
-	for (i = 1; i < 50; i++)
+	for (i = 0; i < 50; i++)
 	{
-		if (i == 1)
-			printf("%d", f1);
-		else if (i == 2)
-			printf("%d", f2);
-
 		sum = f1 + f2;
+		printf("%d", sum);
+
 		f1 = f2;
 		f2 = sum;
 
-		printf("%d", sum);
+		if (i == 49)
+			printf("\n");
+		else 
+			printf(", ");
 	}
 	return (0);
 }
