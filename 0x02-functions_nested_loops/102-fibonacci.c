c556fa8e98f4e6479db8575eede9a28391fb1df1
mohameds <mohmedahmed557766@gmail.com>
2023-03-21 23:45:05 -0700

    fibonacci

---
+++ 0x02-functions_nested_loops/102-fibonacci.c
@@ -0,0 +1,26 @@
+#include "main.h"
+
+/**
+ * main - check the code.
+ *
+ * Return: Always 0.
+*/
+int main(void)
+{
+	int i, f1 = 0, f2 = 1, sum;
+
+	for (i = 1; i < 50; count++)
+	{
+		if (i == 1)
+			printf("%d", f1);
+		else if (i == 2)
+			printf("%d", f2);
+
+		sum = f1 + f2;
+		f1 = f2;
+		f2 = sum;
+
+		printf("%d", sum);
+	}
+	return (0);
+}
