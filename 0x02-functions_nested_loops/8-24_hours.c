b8f799c65b63a65baa8ff03271cca2a27f59e6ff
mohameds <mohmedahmed557766@gmail.com>
2023-03-21 22:09:52 -0700

    jack_bauer

---
+++ 0x02-functions_nested_loops/8-24_hours.c
@@ -0,0 +1,21 @@
+#include "main.h"
+
+/**
+ * jack_bauer - print every houre and minute
+*/
+void jack_bauer(void)
+{
+	int hr, min;
+
+	for (hr = 0; hr < 24; hr++)
+	{
+		for (min = 0; min < 60; min++)
+		{
+			putchar ((hr / 10) + '0');
+			putchar ((hr % 10) + '0');
+			putchar (':');
+			putchar ((min / 10) + '0');
+			putchar ((min % 10) + '0');
+		}
+	}
+}
