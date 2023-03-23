ca0e0b39da25e5b766c273ade0ab400e01e9f296
mohameds <mohmedahmed557766@gmail.com>
2023-03-23 09:57:12 -0700

    task4

---
+++ 0x04-more_functions_nested_loops/4-print_most_numbers.c
@@ -0,0 +1,22 @@
+#include "main.h"
+
+/**
+ * print_most_numbers - prints the numbers, from 0 to 9
+ *
+ * Return: Always 0.
+*/
+
+void print_most_numbers(void)
+{
+	char c[] = "0123456789";
+	int i;
+
+	for (i = 0; i < 10; i++)
+	{
+		if (i != 2 && i != 4)
+		{
+			_putchar(c[i]);
+		}
+	}
+	_putchar('\n');
+}
